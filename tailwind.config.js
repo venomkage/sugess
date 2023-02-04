356d1a8bbc2a93931ec96843ce59d6519fe4fda5
venomkage <ceomindset3@gmail.com>
2023-02-04 17:15:10 +0530

    Tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
