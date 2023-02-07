9b8da11626bb9859f1115eb9b8294578d0d93f95
venomkage <ceomindset3@gmail.com>
2023-02-07 13:36:38 +0530

    MVP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    API_KEY: process.env.API_KEY,
+  },
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
